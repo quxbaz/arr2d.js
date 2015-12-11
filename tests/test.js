1399ecb5b30aff84bb3c9bcd2d0710bac092a37a
David Yeung <quxbaz@gmail.com>
2015-12-11 15:12:48 -0500

    Added a describe block and compose function.

--- tests/test.js
+++ tests/test.js
@@ -6,6 +6,20 @@
 var should = require('./chai').should();
 var Array2d = require('../arr2d');
 
+/*
+  Used to render functions that throw and error testable.
+
+  Usage:
+  var add = function(x, y) {return x + y;};
+  var fn = compose(add)(2, 3);
+  fn() -> 6
+*/
+function compose(fn, context) {
+  return function() {
+    return fn.apply(context, arguments);
+  }
+}
+
 // Make a test object; saves on typing.
 function obj(w, h) {
   var w = w | 0;
@@ -13,36 +27,51 @@ function obj(w, h) {
   return new Array2d(w, h);
 }
 
-it("Should create an Array2d instance with no parameters.", function() {
+describe('Array2d', function() {
+
+  it("should create an instance with no parameters.", function() {
   var arr = obj();
-  arr.len().should.eql(0);
-});
+    arr.len().should.eql(0);
+  });
 
-it("Should create an Array2d instance with width and height.", function() {
-  obj(2, 3);
-});
+  it("should create an instance with width and height.", function() {
+    obj(2, 3);
+  });
 
-it("Should confirm length.", function() {
-  var arr = obj(2, 3);
-  arr.len().should.be.eql(6);
-});
+  it("should confirm length.", function() {
+    var arr = obj(2, 3);
+    arr.len().should.be.eql(6);
+  });
 
-it("Should return true on inside bounds.", function() {
-  var arr = obj(2, 3);
-  arr.isBounded([0, 0]).should.be.true;
-  arr.isBounded([0, 1]).should.be.true;
-  arr.isBounded([1, 0]).should.be.true;
-  arr.isBounded([1, 2]).should.be.true;
-});
+  it("should return true on inside bounds.", function() {
+    var arr = obj(2, 3);
+    arr.isBounded([0, 0]).should.be.true;
+    arr.isBounded([0, 1]).should.be.true;
+    arr.isBounded([1, 0]).should.be.true;
+    arr.isBounded([1, 2]).should.be.true;
+  });
 
-it("Should return false on outside bounds.", function() {
-  var arr = obj(2, 3);
-  arr.isBounded([-1, 0]).should.be.false;
-  arr.isBounded([0, -1]).should.be.false;
-  arr.isBounded([2, 3]).should.be.false;
-});
+  it("should return false on outside bounds.", function() {
+    var arr = obj(2, 3);
+    arr.isBounded([-1, 0]).should.be.false;
+    arr.isBounded([0, -1]).should.be.false;
+    arr.isBounded([2, 3]).should.be.false;
+  });
 
-it("Should return check if obj is empty.", function() {
-  var arr = obj(0, 0);
-  arr.isEmpty().should.be.true;
+  it("should return check if obj is empty.", function() {
+    var arr = obj(0, 0);
+    arr.isEmpty().should.be.true;
+  });
+
+  it("should check .get for bounds.", function() {
+    var arr = obj(2, 3);
+    arr.get([0, 0]);
+    arr.get([0, 1]);
+    arr.get([1, 0]);
+    arr.get([1, 2]);
+    arr.get([-1, 0]);
+    // arr.get([0, -1]).should.throw();
+    // arr.get([2, 3]).should.throw();
+  });
+
 });
